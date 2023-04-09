ab5d30395a8829afbf7b592271f0ca9a26060c14
unpaleness <generalissimusunpaleness@gmail.com>
2023-04-09 11:53:55 +0200

    the-labyrinth: search works

--- the-labyrinth.cpp
+++ the-labyrinth.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <queue>
@@ -8,45 +9,113 @@
 
 namespace {
 
-const uint32_t DIST_NONE{-1};
-
-class Node;
+const uint32_t DIST_NONE{0};
 
 struct Point {
     uint32_t x{0};
     uint32_t y{0};
 
     bool operator==(const Point& other) const { return x == other.x && y == other.y; }
+
+    std::string toString() const { return std::to_string(x) + " " + std::to_string(y); }
 };
 
-template <>
-struct std::hash<Point> {
-    size_t operator()(cosnt Point& p) const noexcept {
+struct PointHash {
+    size_t operator()(const Point& p) const noexcept {
         return static_cast<size_t>(p.x) + (static_cast<size_t>(p.y) << 32);
     }
+};
+
+inline bool IsValidPoint(const std::vector<std::string>& field, const Point& p) {
+    return p.y < field.size() && p.x < field[0].size();
+}
+
+inline bool IsSpace(const std::vector<std::string>& field, const Point& p) {
+    return IsValidPoint(field, p) && field[p.y][p.x] != '?' && field[p.y][p.x] != '#';
+}
+
+class Node {
+public:
+    Node(const Point& aPos, const uint32_t aDistance, const std::shared_ptr<Node>& aParent = nullptr)
+        : pos(aPos), distance(aDistance), parent(aParent) {}
+
+    Point getPos() const { return pos; }
+    uint32_t getDistance() const { return distance; }
+
+    std::string toString() const { return pos.toString() + " " + std::to_string(distance); }
+
+private:
+    std::shared_ptr<Node> parent;
+    Point pos;
+    uint32_t distance{DIST_NONE};
 };
 
 class PathTree {
 public:
     PathTree(const std::vector<std::string>& field, const Point& start, const Point& end) {
-        std::unordered_set<size_t> visitedNodeHashes;
-        std::queue<std::shared_ptr<Node>> nodesToVisit;
+        tryFindPath(field, start, end);
+        printPathTree(field);
+    }
+
+    void printPathTree(const std::vector<std::string>& field) const {
+        auto fieldTmp = field;
+        for (const auto& node : nodes) {
+            if (!node) {
+                continue;
+            }
+            const auto pos = node->getPos();
+            if (!IsValidPoint(fieldTmp, pos)) {
+                continue;
+            }
+            fieldTmp[pos.y][pos.x] = 0x30 + node->getDistance() % 10;
+        }
+        for (const auto& row : fieldTmp) {
+            std::cout << row << std::endl;
+        }
     }
 
 private:
+    bool tryFindPath(const std::vector<std::string>& field, const Point& start, const Point& end) {
+        root = nullptr;
+        goal = nullptr;
+        nodes.clear();
+        if (!IsValidPoint(field, start)) {
+            return false;
+        }
+        std::unordered_set<Point, PointHash> visitedPoints;
+        std::queue<std::shared_ptr<Node>> nodesToVisit;
+        root = std::make_shared<Node>(start, 0);
+        nodes.push_back(root);
+        if (IsSpace(field, start)) {
+            nodesToVisit.push(root);
+        }
+        while (!nodesToVisit.empty()) {
+            auto node = nodesToVisit.front();
+            nodesToVisit.pop();
+            if (!node) {
+                continue;
+            }
+            // std::cout << "node: " << node->toString() << ", queue: " << nodesToVisit.size() << std::endl;
+            const auto pos = node->getPos();
+            if (pos == end) {
+                goal = node;
+            }
+            visitedPoints.insert(pos);
+            for (const auto p :
+                 {Point{pos.x - 1, pos.y}, Point{pos.x + 1, pos.y}, Point{pos.x, pos.y - 1}, Point{pos.x, pos.y + 1}}) {
+                if (!IsSpace(field, p) || visitedPoints.count(p)) {
+                    continue;
+                }
+                const auto newNode = std::make_shared<Node>(p, node->getDistance() + 1, node);
+                nodesToVisit.push(newNode);
+                nodes.push_back(newNode);
+            }
+        }
+        return !!goal;
+    }
     std::shared_ptr<Node> root{nullptr};
-};
-
-class Node {
-public:
-    Node() { daughters.reserve(4); }
-
-private:
-    char type{'?'};
-    std::vector<std::shared_ptr<Node>> daughters;
-    std::weak_ptr<Node> parent{nullptr};
-    Point pos;
-    uint32_t distance{DIST_NONE};
+    std::shared_ptr<Node> goal{nullptr};
+    std::vector<std::shared_ptr<Node>> nodes;
 };
 
 }  // namespace
@@ -62,10 +131,11 @@ int main() {
     uint32_t a;  // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
     std::cin >> r >> c >> a;
     std::cin.ignore();
-    std::vector<std::string> field(r, string(c, '?'));
+    std::vector<std::string> field(r, std::string(c, '?'));
 
+    uint32_t iteration{0};
     // game loop
-    while (1) {
+    while (iteration < 2) {
         uint32_t kr;  // row where Rick is located.
         uint32_t kc;  // column where Rick is located.
         std::cin >> kr >> kc;
@@ -77,9 +147,13 @@ int main() {
             field[i] = row;
         }
 
+        PathTree pathTree(field, Point{kc, kr}, Point{});
+
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
 
         std::cout << "RIGHT" << std::endl;  // Rick's next move (UP DOWN LEFT or RIGHT).
+
+        ++iteration;
     }
 }
